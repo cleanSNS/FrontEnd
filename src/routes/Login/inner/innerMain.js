6c435ee4828add5968d29db9cd59ea76cc95c70d
Ben <luckhome5@naver.com>
2022-07-27 22:01:17 +0900

    login page kakao, naver added ...

--- src/routes/Login/inner/innerMain.js
+++ src/routes/Login/inner/innerMain.js
@@ -1,9 +1,18 @@
 //로그인의 초기 화면. id, 비번을 쓰는 곳이 있다.
 import { useState } from 'react';
+import Style from './innerMain.module.css';
+import KakaoImg from '../socialImg/Kakao.png';
+import NaverImg from '../socialImg/Naver.png';
+import axios from 'axios';
+
+const loginApiUrl = 'http://13.209.50.133:8080/user/auth/login';
+const kakaoLoginApiUrl = 'http://13.209.50.133:8080/social/login/kakao/code';
+const naverLoginApiUrl = 'http://13.209.50.133:8080/social/login/naver/code';
 
 const Main = ({changeContent}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [wrong, setWrong] = useState("0");
     const emailHandler = (event) =>{
         event.preventDefault();
         setEmail(event.target.value);
@@ -14,22 +23,45 @@ const Main = ({changeContent}) => {
     };
     const submitHandler = (event) => {
         event.preventDefault();
-        if(email === '' || password === '') return
+        if(email === '' || password === '') return;
 
-        let loginInfo ={ //보내는 객체 내용 달라질 수 있음 백이랑 통일해야함
+        axios.post(loginApiUrl, {
             email: email,
             password: password,
-        };
-
-        window.location.href = '/home';//구현만 이렇게 해놓음 나중에는 아래껄로 해야함
-
-//        fetch('...', loginInfo)
-//            .then((response) => response.json())
-//            .then((res) => {
-//                console.log(res);
-//                res.message === '...' ? alert("아이디나 비밀번호가 틀렸습니다") : window.locatin.href='/home';
-//            });
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res);
+                if(res.message === undefined){
+                    setWrong("0");
+                    window.location.href = '/home';
+                }
+                else{
+                    setWrong("1");
+                    setWrong("2");
+                }
+            });
     };
+    const kakaoLoginHandler = (event) => {
+        event.preventDefault();
+        console.log("kakao");
+
+        axios.get(kakaoLoginApiUrl)
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res);
+            })
+
+    }
+    const naverLoginHandler = (event) => {
+        event.preventDefault();
+        console.log("naver");
+        axios.get(naverLoginApiUrl)
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res);
+            })
+    }
     return(
         <div className="container d-flex flex-column justify-content-center align-items-center">
             <div className="row w-75">
@@ -39,6 +71,10 @@ const Main = ({changeContent}) => {
                     </div>
                     <div className="col-12">
                         <input type="password" placeholder="Password" className="form-control my-1" value={password} onChange={passwordHandler}/>
+                    </div>
+                    <div className="col-12 d-flex flex-column justify-content-center align-items-center my-4">
+                        {wrong === "1" ? <p className="text-danger">잘못된 이메일 혹은 비밀번호입니다.</p> : null}
+                        {wrong === "2" ? <p className="text-danger">인증 아직 완료되지 않은 계정입니다.</p> : null}
                     </div>
                     <div className="col-12 d-flex flex-column justify-content-center align-items-center my-4">
                         <button className="btn btn-dark w-50 shadow" type="submit">로그인</button>
@@ -51,6 +87,24 @@ const Main = ({changeContent}) => {
                 </div>
                 <div className="col-6 d-flex flex-column justify-content-center align-items-center">
                     <a href="" className="text-dark text-center text-decoration-none" onClick={changeContent} name="2">회원 가입</a>
+                </div>
+            </div>
+            <div className="row w-75 my-5">
+                <div className="col-6">
+                    <button className="btn btn-transparent p-0 border border-0 ">
+                        <div id="KakaoContainer" className={Style.kakao}>
+                            <img id="symbol" src={KakaoImg} className={Style.kakaoSymbol}/>
+                            <span id="label" className={Style.kakaoLabel} onClick={kakaoLoginHandler}>로그인</span>
+                        </div>
+                    </button>
+                </div>
+                <div className="col-6">
+                    <button className="btn btn-transparent p-0 border border-0 ">
+                        <div id="NaverContainer" className={Style.naver}>
+                            <img id="symbol" src={NaverImg} className={Style.naverSymbol}/>
+                            <span id="label" className={Style.naverLabel} onClick={naverLoginHandler}>로그인</span>
+                        </div>
+                    </button>
                 </div>
             </div>
         </div>
--- src/routes/Login/inner/innerSignUp.js
+++ src/routes/Login/inner/innerSignUp.js
@@ -1,5 +1,6 @@
 //회원가입 부분
 import {useState, useEffect} from 'react';
+const signUpApiUrl = 'http://13.209.50.133:8080/user/auth/signup';
 
 const SignUp = ({changeContent}) => {
     const [email, setEmail] = useState("");
@@ -56,33 +57,37 @@ const SignUp = ({changeContent}) => {
     const genderAgreeHandler = () => {
         setGenderAgree((current)=> !current);
     }
-    const submitHandler = (event) => {
-        event.preventDefault();
-        if(email === '' || password === '' || passwordCheck === '' || nickname === '' || age === 0 || gender === '') return
-        if(password !== passwordCheck) return
-
-        let signUpInfo = {
-            email: email,
-            password: password,
-            nickname: nickname,
-            age: age,
-            ageagree: ageAgree,
-            gender: gender,
-            genderagree: genderAgree,
-        };
-
-        window.location.href = '/home';//구현만 이렇게 해놓음 나중에는 아래껄로 해야함
-
-//        fetch('...', signUpInfo)
-//            .then((response) => response.json())
-//            .then((res) => {
-//                console.log(res);
-//                res.message === '...' ? alert("이미 가입된 아이디입니다.") : window.locatin.href='/home';
-//            });
-    }
-
     const onfocusHandler = (event) => {
         event.target.select();
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if(email === '' || password === '' || passwordCheck === '' || nickname === '' || age === 0 || gender === '') return;
+        if(password !== passwordCheck) return;
+
+        fetch(signUpApiUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: email,
+                password: password,
+                nickname: nickname,
+                age: age,
+                gender: gender,
+                ageVisible: ageAgree,
+                genderVisible: genderAgree,
+            }),
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res);
+                if(res.message === undefined){
+                    //지금은 로그인 화면으로 이동 나중에 달라질 수 있음
+                    window.location.href = '/login';
+                }
+                else{
+                    alert("제출 정보에 문제가 있습니다. 다시 한 번 확인해주십시오.");
+                }
+            });
     }
     return(
         <div className="container bg-light border rounded-3 shadow">
@@ -91,42 +96,42 @@ const SignUp = ({changeContent}) => {
                 <legend>
                     <p className="my-1 my-xxl-4 d-flex flex-column justify-content-center align-items-center fw-bold">회 원 가 입</p>
                 </legend>
-                <div className="col-12 my-xxl-3">
+                <div className="col-12 my-xxl-3 my-1">
                     <input id="emailInput" className="form-control shadow-sm" type="email" placeholder="Email" value={email} onChange={emailHandler} onFocus={onfocusHandler} />
                 </div>
-                <div className="col-12 my-xxl-3">
+                <div className="col-12 my-xxl-3 my-1">
                     <input id="passwordInput" className="form-control shadow-sm" type="password" placeholder="Password" value={password} onChange={passwordHandler} onFocus={onfocusHandler} />
                 </div>
-                <div className="col-12 my-xxl-3">
+                <div className="col-12 my-xxl-3 my-1">
                     <input id="passwordCheckInput" className="form-control shadow-sm" type="password" placeholder="Password 확인" value={passwordCheck} onChange={passwordCheckHandler} onFocus={onfocusHandler} />
                 </div>
-                <div className="col-12 my-xxl-3">
+                <div className="col-12 my-xxl-3 my-1">
                     <input id="nicknameInput" className="form-control shadow-sm" type="text" placeholder="닉네임" value={nickname} onChange={nicknameHandler} onFocus={onfocusHandler} />
                 </div>
 
-                <div className="col-6 col-xxl-4 my-xxl-3">
+                <div className="col-6 col-xxl-4 my-xxl-3 my-1">
                     <input id="ageInput" className="form-control shadow-sm" type="number" placeholder="나이" value={age} onChange={ageHandler} onFocus={onfocusHandler} />
                 </div>
-                <div className="form-check col-6 col-xxl-3 my-xxl-3">
+                <div className="form-check col-6 col-xxl-3 my-xxl-3 my-1">
                     <input id="ageAgree" type="checkbox" className="form-check-input shadow-sm" onChange={ageAgreeHandler}/>
-                    <label className="form-check-label" htmlFor="ageAgree">공개 여부</label>
+                    <label className="form-check-label " htmlFor="ageAgree">공개 여부</label>
                 </div>
                 <div className="d-xxl-none col-1" />
-                <div className="form-check col-2 col-xxl-1 my-xxl-3">
-                    <input id="male" className="form-check-input shadow-sm" type="radio" name="gender" onChange={genderHandler}/>
+                <div className="form-check col-2 col-xxl-1 my-xxl-3 my-1">
+                    <input id="MALE" className="form-check-input shadow-sm" type="radio" name="gender" onChange={genderHandler}/>
                     <label className="form-check-label" htmlFor="male">남</label>
                 </div>
-                <div className="form-check col-2 col-xxl-1 my-xxl-3">
-                    <input id="female" className="form-check-input shadow-sm" type="radio" name="gender" onChange={genderHandler} />
+                <div className="form-check col-2 col-xxl-1 my-xxl-3 my-1">
+                    <input id="FEMALE" className="form-check-input shadow-sm" type="radio" name="gender" onChange={genderHandler} />
                     <label className="form-check-label" htmlFor="female">여</label>
                 </div>
                 <div className="d-xxl-none col-1" />
-                <div className="form-check col-6 col-xxl-3 my-xxl-3">
+                <div className="form-check col-6 col-xxl-3 my-xxl-3 my-1">
                     <input id="genderAgree" type="checkbox" className="form-check-input shadow-sm" onChange={genderAgreeHandler}/>
                     <label className="form-check-label" htmlFor="genderAgree">공개 여부</label>
                 </div>
             </fieldset>
-            <div className="row my-xxl-4 my-3">
+            <div className="row my-xxl-4 my-3 my-1">
                 <div className="col-3" />
                 <div className="col-3 d-flex flex-column justify-content-center align-items-center">
                     <button className="btn btn-light shadow-sm" onClick={changeContent} value="0">취소</button>
