dac95643a367e9df5787b540a0844da3dfd994a1
SUH <luckhome5@naver.com>
2022-09-02 13:20:07 +0900

    apiUrl fix 2

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -2,21 +2,21 @@ export const apiUrl = "http://cleanbook.site";
 
 /***************************로그인 관련 URl***********************************/
 //기본 로그인 Url
-export const loginApiUrl = `http://${apiUrl}/user/auth/login`;
+export const loginApiUrl = `${apiUrl}/user/auth/login`;
 
 //카카오 소셜 로그인 Url
-export const kakaoLoginUrl = `http://${apiUrl}/social/login/kakao/code`;
+export const kakaoLoginUrl = `${apiUrl}/social/login/kakao/code`;
 
 //네이버 소셜 로그인 Url
-export const naverLoginApiUrl = `http://${apiUrl}/social/login/naver/code`;
+export const naverLoginApiUrl = `${apiUrl}/social/login/naver/code`;
 
 //회원가입 Url
-export const signUpApiUrl = `http://${apiUrl}/user/auth/signup`;
+export const signUpApiUrl = `${apiUrl}/user/auth/signup`;
 
 //이메일 인증 Url
-export const emailApiUrl = `http://${apiUrl}/user/auth/signup/request`;
+export const emailApiUrl = `${apiUrl}/user/auth/signup/request`;
 
 
 /***************************메인 페이지 관련 URl***********************************/
 //새 글 피드 가져오는 Url
-export const pageloadUrl = `http://${apiUrl}/page/main`;
+export const pageloadUrl = `${apiUrl}/page/main`;
