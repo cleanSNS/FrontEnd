14b4ebddd9fa12b7a03627b69b0025c756ee1630
Ben <luckhome5@naver.com>
2022-09-12 23:03:52 +0900

    newpost 90% done

--- src/routes/Home/leftInner/newPost/newPost.js
+++ src/routes/Home/leftInner/newPost/newPost.js
@@ -1,11 +1,10 @@
 import Style from './newPost.module.css';
 import { useState } from 'react';
+import addImage from '../../root/tagImages/add.png';
 
 /*
 
 지금 남은 할 일
-
-해시태그 지웠을 때 즉각적으로 지워지는지 확인하기
 
 이미지 드래그 앤 드롭하는 부분 다듬기 - CSS
 이미지 드래그 앤 드롭으로 이미지 입력 받기
@@ -15,9 +14,19 @@ import { useState } from 'react';
 
 */
 
-
-
-
+const HashtagList = ({deleteTag, newPostHashtag}) => {
+    return (
+        <div className={Style.Cover}>
+            {
+                newPostHashtag.map((data, index) =>(
+                    <button className={Style.singleHashTag} onClick={deleteTag} key={index} value={index}>
+                        #{data} 
+                    </button>
+                ))
+            }
+        </div>
+    );
+};
 
 const LeftNewPost = ({ newPostImages, setNewPostImages, newPostHashtag, setNewPostHashtag, newPostContent, setNewPostContent, uploadNewPostHandler }) => {
     const [hashtag, setHashtag] = useState("");//임시로 입력되는 값 변경하는 State.
@@ -27,6 +36,39 @@ const LeftNewPost = ({ newPostImages, setNewPostImages, newPostHashtag, setNewPo
         event.preventDefault();
         setNewPostContent(event.target.value);
     }
+
+    //이미지 영역에 파일을 드랍한 경우 - ondrop
+    const imageDropHandler = (event) => {
+        event.preventDefault();
+
+        const files = [...event.dataTransfer?.files];
+        console.log(files);
+
+        const imageUploadArea = document.querySelector("#imageUploadArea");
+        imageUploadArea.style.backgroundColor="white";
+        imageUploadArea.style.border="5px dashed rgb(190, 190, 190)";
+    };
+
+    //이미지 영역위에 파일을 올려놓은 경우 - ondragover(이게 있어야 ondrop이 활성화 된다.)
+    const imageDragOverHandler = (event) => {
+        event.preventDefault();
+    };
+
+    //파일을 이미지 영역 위로 최초 진입한 경우 - ondragenter
+    const imageDragEnterHandler = (event) => {
+        event.preventDefault();
+        const imageUploadArea = document.querySelector("#imageUploadArea");
+        imageUploadArea.style.backgroundColor="rgb(236, 236, 236)";
+        imageUploadArea.style.border="5px dashed rgb(150, 150, 150)";
+    };
+
+    //파일을 이미지 영역 위에서 벗어나게 한 경우 - ondragleave
+    const imageDragLeaveHandler = (event) => {
+        event.preventDefault();
+        const imageUploadArea = document.querySelector("#imageUploadArea");
+        imageUploadArea.style.backgroundColor="white";
+        imageUploadArea.style.border="5px dashed rgb(190, 190, 190)";
+    };
 
     //hashtag영역 바꿔주는 함수
     const hashtagHandler = (event) => {
@@ -38,8 +80,12 @@ const LeftNewPost = ({ newPostImages, setNewPostImages, newPostHashtag, setNewPo
         }
         if(value[value.length - 1] === ","){// 이 경우 
             const tmp = newPostHashtag;
-            tmp.push(value.slice(0,-1));
-            setNewPostHashtag(tmp);
+            const input = value.slice(0,-1);
+            if(!newPostHashtag.includes(input)){
+                tmp.push(input);
+                setNewPostHashtag(tmp);
+                setHashtag("");
+            }
             setHashtag("");
         }
         else{
@@ -50,7 +96,7 @@ const LeftNewPost = ({ newPostImages, setNewPostImages, newPostHashtag, setNewPo
     //hashtag 지우는 함수
     const deleteTag = (event) => {
         event.preventDefault();
-        const tmp = newPostHashtag;
+        const tmp = [...newPostHashtag];
         tmp.splice(Number(event.target.value), 1);
         setNewPostHashtag(tmp);
     };
@@ -58,59 +104,41 @@ const LeftNewPost = ({ newPostImages, setNewPostImages, newPostHashtag, setNewPo
     return(
         <form className={Style.WholeCover} onSubmit={uploadNewPostHandler}>
             {/* 드래그 앤 드롭 영역 */}
-            <div className={Style.Cover}>
-                <div className={Style.picture}>
-                    이미지를 드래그 앤 드롭 하시거나
-                    여기를 눌러 시작하세요.
-                </div>
+            <div className={Style.picture} id="imageUploadArea" onDrop={imageDropHandler} onDragOver={imageDragOverHandler} onDragEnter={imageDragEnterHandler} onDragLeave={imageDragLeaveHandler}>
+                <img src={addImage} className={Style.pictureinnerimage} />
+                <p className={Style.pictureinnerword}>업로드할 이미지를 여기로 옮겨주세요.</p>
+            </div>
+            {/* 올린 이미지 미리 보기 영역 */}
+            <div className={Style.ListArea}>
+
             </div>
             {/* hashtag label 영역 */}
-            <div className={Style.Cover}>
-                <p className={Style.hashtag}>키워드 (,로 분리해주세요)</p>
-            </div>
+            <p className={Style.hashtag}>키워드 (,로 분리해주세요)</p>
             {/* hashtag input 영역 */}
-            <div className={Style.Cover}>
-                <input 
-                    type="text"
-                    value={hashtag}
-                    onChange={hashtagHandler}
-                    className={Style.hashtagInput}
-                />
-            </div>
+            <input 
+                type="text"
+                value={hashtag}
+                onChange={hashtagHandler}
+                className={Style.hashtagInput}
+            />
             {/* hashtag list 영역 */}
-            <div className={Style.Cover}>
-                <div className={Style.tagListArea}>
-                    {
-                    <div className={Style.Cover}>
-                        {
-                            newPostHashtag.map((data, index) =>(
-                                <button className={Style.singleHashTag} onClick={deleteTag} key={index} value={index}>
-                                    #{data} 
-                                </button>
-                            ))
-                        }
-                    </div>
-                    }
-                </div>
+            <div className={Style.ListArea}>
+                <HashtagList deleteTag={deleteTag} newPostHashtag={newPostHashtag}/>
             </div>
             {/* word label 영역 */}
-            <div className={Style.Cover}>
-                <p className={Style.word}>글을 입력해 주세요</p>
-            </div>
+            <p className={Style.word}>글을 입력해 주세요</p>
             {/* word input 영역 */}
-            <div className={Style.Cover}>
-                <input 
-                    type="text" 
-                    vaslue={newPostContent}
-                    onChange={contentHandler}
-                    className={Style.wordInput} />
-            </div>
+            <input 
+                type="text" 
+                vaslue={newPostContent}
+                onChange={contentHandler}
+                className={Style.wordInput} />
             {/* 글 제출 영역 */}
-            <div className={Style.Cover}>
+            <div className={Style.submitbtnArea}>
                 <button type="submit" className={Style.submitButton}>Submit</button>
             </div>
         </form>
     );
-}
+};
 
 export default LeftNewPost;
