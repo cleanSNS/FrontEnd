663e125ad116e82233a05d671059d3eac6404cfd
Ben <luckhome5@naver.com>
2022-10-01 17:19:23 +0900

    bug fix

--- src/routes/Home/leftInner/page/page.js
+++ src/routes/Home/leftInner/page/page.js
@@ -13,6 +13,7 @@ import {
 } from "../../../../apiUrl";
 
 const Pages = ({pageList, lastPage}) => {
+    console.log(pageList);
     return(
         <div className={Style.pageListArea}>
             {
@@ -59,53 +60,7 @@ const Pages = ({pageList, lastPage}) => {
 
 const LeftPage = ({refreshAccessToken}) => {
     const [pageStartId, setPageStartId] = useState(987654321);//글 리스트의 startId
-    const [pageList, setPageList] = useState([
-        {
-            "pageDto": {
-                "userDto": {
-                    "userId": 1,
-                    "nickname": "홍길동",
-                    "imgUrl": null
-                },
-                "pageId": 2,
-                "content": "내용2ㄻㄴㅇ리타퍼ㅣㅏ멎ㄷ;ㅐ랴;ㅂ잭푸ㅐㄴㅇ랴;퍼ㅣㅁㄷ구ㅡ피ㅏㅡㄴ이라ㅓㅏㅣㅁㄴㅇㄻㄴㅇㄻㄴㅇㄹ",
-                "likeCount": 0,
-				"likeReadAuth": false,
-                "createdDate": null
-            },
-            "imgUrlList": []
-        },
-        {
-            "pageDto": {
-                "userDto": {
-                    "userId": 1,
-                    "nickname": "홍길동",
-                    "imgUrl": null
-                },
-                "pageId": 1,
-                "content": "내용1ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄼㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱ",
-                "likeCount": 0,
-				"likeReadAuth": true,
-                "createdDate": null
-            },
-            "imgUrlList": []
-        },
-        {
-            "pageDto": {
-                "userDto": {
-                    "userId": 1,
-                    "nickname": "홍길동",
-                    "imgUrl": null
-                },
-                "pageId": 1,
-                "content": "내용3ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻㄴㅇㄻsdㄴㅇㄼㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱ",
-                "likeCount": 0,
-				"likeReadAuth": true,
-                "createdDate": null
-            },
-            "imgUrlList": []
-        }
-    ]); //글 리스트
+    const [pageList, setPageList] = useState([]); //글 리스트
     const [lastPage, inView] = useInView(); //이게 ref된 요소가 화면에 보이면 inView가 true로 변경
     const [isLoadFinish, setIsLoadFinish] = useState(false);//false면 더 이상 로드할 내용이 남은 경우, true면 로드할 내용이 더 없는 경우이다.
 
@@ -114,7 +69,7 @@ const LeftPage = ({refreshAccessToken}) => {
         axios.get(`${pageloadUrl}?startId=${pageStartId}`)
         .then((res) => {
             const cur = [...pageList];
-            const tmp = [res.data.data];
+            const tmp = [...res.data.data];
             if(tmp.length === 0){
                 setIsLoadFinish(true);//더 이상 글이 없는 경우이다.
                 return;
