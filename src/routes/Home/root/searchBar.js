3075b6845eafa4b9895491e530bd09606e2c3248
Ben <luckhome5@naver.com>
2022-10-03 22:18:32 +0900

    for test

--- src/routes/Home/root/searchBar.js
+++ src/routes/Home/root/searchBar.js
@@ -3,6 +3,13 @@ import Style from './searchBar.module.css';
 
 //나중에 a안에 이미지로 변경하기. 그 이미지 스타일도 만들어야함 반응형으로 만들면 좋을듯
 const SearchBar = ({userSearch, userSearchChangeHandler, userSearchSubmitHandler}) =>{
+    const mouseOverHandler = (event) => {
+        event.target.style.backgroundColor = "gray";
+    }
+    const mouseOutHandler = (event) => {
+        event.target.style.backgroundColor="white"
+    }
+
     return(
         <form className={Style.searchBarCover} onSubmit={userSearchSubmitHandler}>
             <input 
@@ -12,6 +19,12 @@ const SearchBar = ({userSearch, userSearchChangeHandler, userSearchSubmitHandler
                 value={userSearch}
                 onChange={userSearchChangeHandler}
             />
+            <div className={Style.dropBox}>
+                <div className={Style.dropBoxInner}>
+                    <div className={Style.content} onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}/>
+                    <div className={Style.content} onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}/>
+                </div>
+            </div>
         </form>
     )
 }
