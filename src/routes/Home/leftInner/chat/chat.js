facf10d020e4675bd0b7ba7615bac02bb43f127a
Ben <luckhome5@naver.com>
2022-10-14 22:43:42 +0900

    chat trial 19

--- src/routes/Home/leftInner/chat/chat.js
+++ src/routes/Home/leftInner/chat/chat.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 import { Temporal } from '@js-temporal/polyfill';
 import SockJS from 'sockjs-client';
 import Stomp from 'stomp-websocket';
+import userEvent from '@testing-library/user-event';
 
 const SingleChat = ({data, setLeftBookState, userId}) => {
     //유저의 이미지나 이름을 클릭하면 해당 유저의 페이지로 이동한다. <---------------이동이 있는 곳!
@@ -106,14 +107,18 @@ const LeftChat = ({refreshAccessToken, leftBookState, setLeftBookState, userId})
     const socketConnect = () => {
         const socket = new SockJS("https://api.cleanbook.site/ws");
         const tmp = Stomp.over(socket);
-        tmp.connect({}, function (frame) {
-            tmp.subscribe(`https://api.cleanbook.site/sub/${chattingRoomId}`, function (chatMessage) {//구독
+        setStompClient(tmp);
+    };
+
+    useEffect(() => {
+        if(stompClient === null) return; //초기 상황에는 그냥 종료
+        stompClient.connect({}, function (frame) {
+            stompClient.subscribe(`https://api.cleanbook.site/sub/${chattingRoomId}`, function (chatMessage) {//구독
                 console.log(JSON.parse(chatMessage.body));//chatMessage.body
                 console.log("되는지 정말 궁금하다.");
             });
         });
-        setStompClient(tmp);
-    };
+    }, [stompClient]);
 
     //가장 먼저 채팅방의 아이디를 가져온다.
     const presetChattingRoomId = () => {
