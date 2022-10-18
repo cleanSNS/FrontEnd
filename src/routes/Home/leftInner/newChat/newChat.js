0bdb2b5699a2a1b2fae603686021fa4220e922f7
SUH <luckhome5@naver.com>
2022-10-18 15:20:23 +0900

    chat trial 25

--- src/routes/Home/leftInner/newChat/newChat.js
+++ src/routes/Home/leftInner/newChat/newChat.js
@@ -133,7 +133,7 @@ const LeftNewChat = ({refreshAccessToken, setLeftBookState, userId}) => {
         chosenFriendUserIdList.push(userId);//나도 집어넣는다.
         let chattingRoomName = chattingRoomNameInput;//채팅방 이름 설정
         if(chattingRoomName === ""){//사용자가 채팅방 명을 정해주지 않은 경우 이름 자동 생성
-            chattingRoomName = `${chosenFriendList[0].nickname}외${chosenFriendList.length - 1}명`
+            chattingRoomName = `${chosenFriendList[0].nickname}외${chosenFriendUserIdList.length}명`
         }
 
         axios.post(makeNewChattingRoomUrl, {
