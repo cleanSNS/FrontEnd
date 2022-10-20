15e1b30c936f6cbd4996bc6bc89ed43f0febcec5
SUH <luckhome5@naver.com>
2022-10-20 15:08:17 +0900

    comment of comment like

--- src/routes/Home/root/detailPage.js
+++ src/routes/Home/root/detailPage.js
@@ -51,55 +51,101 @@ const calculateTimeFrom = (calTime) => {
 
 //대댓글 부분
 const SingleCommentOfComment = ({data, lastCommentOfComment, setPageId, userId, refreshAccessToken, pageId, leftBookChangeHandler, setToggle, setCommentOfCommentList, setCommentOfCommentStartId, setIsLastCommentOfComment}) => {
+    const [COCIsLiked, setCOCIsLiked] = useState(false);//대댓글 좋아요 여부
+    const [COCLikeCount, setCOCLikeCount] = useState(0);//대댓글 좋아요 개수
+
     const CommentofCommentUserClickHandler = () => {
         setPageId(-1);//현재 페이지에서 나감
         leftBookChangeHandler(`pList/${data.userDto.userId}`);//해당 유저의 페이지로 이동
     };
 
-        //신고 클릭함수
-        const COCreportClickHandler = () => {
-            if(window.confirm("정말 신고하시겠습니까?")){//다시 한 번 물어보고 실행
-                axios.post(ReportUrl, {
-                    targetId: data.commentId,
-                    type: "COMMENT",
-                })
-                .then((res) => {
-                    alert("신고가 접수되었습니다.");
-                })
-                .catch((res) => {
-                    if(res.response.status === 401){
-                        refreshAccessToken();
-                    }
-                    else{
-                        console.log(res);
-                        alert("신고하지 못했습니다.");
-                    }
-                });
-            }
+    //신고 클릭함수
+    const COCreportClickHandler = () => {
+        if(window.confirm("정말 신고하시겠습니까?")){//다시 한 번 물어보고 실행
+            axios.post(ReportUrl, {
+                targetId: data.commentId,
+                type: "COMMENT",
+            })
+            .then((res) => {
+                alert("신고가 접수되었습니다.");
+            })
+            .catch((res) => {
+                if(res.response.status === 401){
+                    refreshAccessToken();
+                }
+                else{
+                    console.log(res);
+                    alert("신고하지 못했습니다.");
+                }
+            });
         }
+    };
     
-        //삭제 클릭 함수
-        const COCdeleteClickHandler = () => {
-            if(window.confirm("정말 삭제하시겠습니까?")){
-                axios.delete(`${deleteCommentUrl}${pageId}/comment/${data.commentId}`)
-                .then((res) => {
-                    alert("삭제되었습니다.");
-                    setCommentOfCommentStartId(1)//대댓글 startId 초기화
-                    setIsLastCommentOfComment(false)//대댓글 lastcomment fasle
-                    setToggle(false)//토글 값 강제로 false로
-                    setCommentOfCommentList([])//대댓글 지우기
-                })
-                .catch((res) => {
-                    if(res.response.status === 401){
-                        refreshAccessToken();
-                    }
-                    else{
-                        console.log(res);
-                        alert("삭제하지 못했습니다.");
-                    }
-                })
-            }
+    //삭제 클릭 함수
+    const COCdeleteClickHandler = () => {
+        if(window.confirm("정말 삭제하시겠습니까?")){
+            axios.delete(`${deleteCommentUrl}${pageId}/comment/${data.commentId}`)
+            .then((res) => {
+                alert("삭제되었습니다.");
+                setCommentOfCommentStartId(1)//대댓글 startId 초기화
+                setIsLastCommentOfComment(false)//대댓글 lastcomment fasle
+                setToggle(false)//토글 값 강제로 false로
+                setCommentOfCommentList([])//대댓글 지우기
+            })
+            .catch((res) => {
+                if(res.response.status === 401){
+                    refreshAccessToken();
+                }
+                else{
+                    console.log(res);
+                    alert("삭제하지 못했습니다.");
+                }
+            })
         }
+    };
+
+    //초기에 좋아요 관련 정보 불러오기
+    useEffect(() => {
+        setCOCLikeCount(data.likeCount);
+        axios.get(`${checkLikeUrl}?targetId=${data.commentId}&type=COMMENT`)
+        .then((res) => {
+            setCOCIsLiked(res.data.data.like);
+        })
+        .catch((res) => {
+            if(res.response.status === 401){
+                refreshAccessToken();
+            }
+            else{
+                console.log(res);
+                alert("삭제하지 못했습니다.");
+            }
+        });
+    }, []);
+
+    //좋아요 clickhandler
+    const CommentOfCommentLikeHandler = () => {
+        let url = ""
+        COCIsLiked ? url = unlikeThisPageUrl : url = likeThisPageUrl
+
+        axios.post(url, {
+            targetId: data.commentId,
+            type: "COMMENT"
+        })
+        .then((res) => {
+            COCIsLiked ? setCOCLikeCount(cur => cur - 1) : setCOCLikeCount(cur => cur + 1) //임시로라도 반영
+            setCOCIsLiked((cur) => !cur);
+            console.log("페이지에 좋아요혹은 좋아요 취소했습니다.");
+        })
+        .catch((res) => {
+            if(res.response.status === 401){
+                refreshAccessToken();
+            }
+            else{
+                console.log(res);
+                alert("좋아요정보를 보내지 못했습니다.");
+            }
+        });
+    }
 
     return(
         <div className={Style.CommentBox} style={{width:"80%"}} ref={lastCommentOfComment}>
@@ -110,8 +156,8 @@ const SingleCommentOfComment = ({data, lastCommentOfComment, setPageId, userId, 
             <p className={Style.commentText}>{data.content}</p>
             <div className={Style.likeTimeArea}>
                 <div className={Style.cover}>
-                    <img src={heartImg} className={Style.buttonImg} />
-                    <p className={Style.likeandCommentCount} style={{cursor: "default"}}>{`좋아요${data.likeCount}개`}</p>
+                    <img src={COCIsLiked ? heartImgFill : heartImg} className={Style.buttonImg} onClick={CommentOfCommentLikeHandler}/>
+                    <p className={Style.likeandCommentCount} style={{cursor: "default"}}>{`좋아요${COCLikeCount}개`}</p>
                     <p className={Style.likeandCommentCount} style={{cursor: "default"}}>|</p>
                     {
                         userId === data.userDto.userId ?
@@ -315,7 +361,7 @@ const RenderComment = ({data, pageId, lastComment, setCommentToWhom, refreshAcce
                 console.log(res);
                 alert("삭제하지 못했습니다.");
             }
-        })
+        });
     }, []);
 
     //좋아요 클릭 handler
