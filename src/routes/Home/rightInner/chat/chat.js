e493575fb94ec87ba5c676031d23bfc5578b1624
Ben <luckhome5@naver.com>
2022-10-23 21:31:05 +0900

    chat if 0 null added

--- src/routes/Home/rightInner/chat/chat.js
+++ src/routes/Home/rightInner/chat/chat.js
@@ -78,7 +78,10 @@ const SingleChattingRoom = ({data, setLeftBookState, refreshAccessToken, presetC
                 <p className={Style.chatName} onClick={chatClickHandler}>{data.name}</p>
                 <p className={Style.lastChat} onClick={chatClickHandler}>{data.lastChat}</p>
                 <div className={Style.flexBoxRight}>
-                    <p className={Style.chatNumber}>{data.uncheckedChatCount}</p>
+                    {
+                        data.uncheckedChatCount === 0 ? null :
+                        <p className={Style.chatNumber}>{data.uncheckedChatCount}</p>
+                    }
                 </div>
             </div>
         </div>
