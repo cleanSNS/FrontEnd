621df6cb34154e3750c10b918f301437682ac3c2
Ben <luckhome5@naver.com>
2022-10-23 15:30:29 +0900

    newPost 2

--- src/routes/Home/leftInner/newPost/newPost.js
+++ src/routes/Home/leftInner/newPost/newPost.js
@@ -18,7 +18,7 @@ const HashtagList = ({deleteTag, newPostHashtag}) => {
 
 const ImageList = ({deleteImage, renderedNewPostImages}) => {
     return (
-        <div style={{width: "100%", height: "100%"}}>
+        <div style={{width: `${renderedNewPostImages.length * 200}px`, height: "100%"}}>
             {
                 renderedNewPostImages.map((data, index) =>(
                     <img className={Style.singlepicture} src={data} key={index} id={index} onClick={deleteImage} />
