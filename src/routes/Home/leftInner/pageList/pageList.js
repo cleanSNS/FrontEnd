c3ecd5589779eb503e1e2ce9a1158b142d36b3f6
SUH <luckhome5@naver.com>
2022-10-25 12:52:36 +0900

    pageList change

--- src/routes/Home/leftInner/pageList/pageList.js
+++ src/routes/Home/leftInner/pageList/pageList.js
@@ -56,7 +56,7 @@ const UserListArea = ({bottomStuff, refreshAccessToken, leftBookChangeHandler, s
 
     const userClickHander = (event) => {
         event.preventDefault();
-        leftBookChangeHandler("pList/" + document.querySelector('[id^=pageListUserId]').id.split('_')[1]);
+        leftBookChangeHandler("pList/" + event.target.id.split('_')[1]);
     };
 
     return(
@@ -64,8 +64,8 @@ const UserListArea = ({bottomStuff, refreshAccessToken, leftBookChangeHandler, s
             {
                 userList.map((data, index) => (
                     <div className={Style.userArea} key={index} onClick={userClickHander} id={`pageListUserId_${data.userId}`}>
-                        <img src={data.imgUrl}className={Style.userImg} />
-                        <p className={Style.userNickname}>{data.nickname}</p>
+                        <img src={data.imgUrl} className={Style.userImg} id={`pageListUserId_${data.userId}`}/>
+                        <p className={Style.userNickname} id={`pageListUserId_${data.userId}`}>{data.nickname}</p>
                     </div>
                 ))
             }
