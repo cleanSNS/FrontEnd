db1b85b8f97baa5fa314428f89fcefb5c3fba5e2
SUH <luckhome5@naver.com>
2022-10-26 16:04:42 +0900

    withdrawal setting 4

--- src/routes/Home/leftInner/setting/withdrawalSetting.js
+++ src/routes/Home/leftInner/setting/withdrawalSetting.js
@@ -44,13 +44,18 @@ const WithdrawalSetting = ({refreshAccessToken, userId, logout}) => {
     return(
         <form className={Style.WholeCover} onSubmit={withdrawalSubmitHandler}>
             <div className={Style.Cover}>
-                <input 
-                    type="password"
-                    placeholder='회원 탈퇴를 위해 비밀번호를 입력해 주세요'
-                    value={userPasswordInput}
-                    onChange={userPasswordInputChangeHandler}
-                    className={Style.settingInput}
-                />
+                <div className={Style.passwordCover}>
+                    <label className={Style.settingLabel}>
+                        비밀번호
+                    </label>
+                    <input 
+                        type="password"
+                        placeholder='회원 탈퇴를 위해 비밀번호를 입력해 주세요'
+                        value={userPasswordInput}
+                        onChange={userPasswordInputChangeHandler}
+                        className={Style.settingInput}
+                    />
+                </div>
             </div>
             <div className={Style.Cover}>
                 <button type='submit' className={Style.submitButton}>제출</button>
