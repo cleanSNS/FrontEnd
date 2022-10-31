cb31f96150d20a7d2538feb447d7ee303e54ab27
SUH <luckhome5@naver.com>
2022-10-31 14:18:54 +0900

    function to new file

---
+++ src/makeStringIntoArray.js
@@ -0,0 +1,20 @@
+export const makeIntoArray = (str) => {//문자열을 받아서 시작부분이 욕설인지 아닌지와, 문자열을 잘라서 구분한 함수이다.
+    const TOKEN = '!모자이크!';
+    let answer = [];
+
+    if(str === null) return ['null'];//이건 문제 해결되면 지우기
+
+    for(let i = 0; i < str.length; i++){
+        const pos = str.indexOf(TOKEN, i);//index가 i인 지점부터 확인하여 시간을 줄인다. 토큰은 6글자이다.
+        if(pos === -1){//더 이상 없는 경우이다.
+            answer.push(str.substring(i, str.length));//앞으로 탐색할 i지점부터 문자열의 길이 까지를 집어넣는다.
+            break;
+        }
+        if(i !== pos) answer.push(str.substring(i,pos));//같은 경우는 i = 0혹은 연속되는 지점에서 발견 가능하다.
+        answer.push(str.substring(pos,pos + TOKEN.length));//!모자이크!를 집어넣는다.
+        i = pos + TOKEN.length;//토큰이 6글자이므로 6이 들어간다.
+    }
+
+    console.log(answer);//확인용 정상작동 되면 지우기
+    return answer;
+};
--- src/routes/Home/leftInner/page/page.js
+++ src/routes/Home/leftInner/page/page.js
@@ -13,6 +13,9 @@ import {
     likeThisPageUrl,
     unlikeThisPageUrl,
 } from "../../../../apiUrl";
+import {
+    makeIntoArray
+} from "../../../../makeStringIntoArray";
 
 const ImageArea = ({imgList, pageIndex, pageClickFunc}) => {
     const [imageIndex, setImageIndex] = useState(0);//보고있는 이미지의 index
@@ -85,27 +88,6 @@ const Pages = ({data, lastPage, index, setPageId, setLeftBookState, refreshAcces
 
     const userProfileClickHandler = () => {
         setLeftBookState(`pList/${data.pageDto.userDto.userId}`);
-    };
-
-    const makeIntoArray = (str) => {//문자열을 받아서 시작부분이 욕설인지 아닌지와, 문자열을 잘라서 구분한 함수이다.
-        const TOKEN = '!모자이크!';
-        let answer = [];
-
-        if(str === null) return ['null'];
-
-        for(let i = 0; i < str.length; i++){
-            const pos = str.indexOf(TOKEN, i);//index가 i인 지점부터 확인하여 시간을 줄인다. 토큰은 6글자이다.
-            if(pos === -1){//더 이상 없는 경우이다.
-                answer.push(str.substring(i, str.length));//앞으로 탐색할 i지점부터 문자열의 길이 까지를 집어넣는다.
-                break;
-            }
-            if(i !== pos) answer.push(str.substring(i,pos));//같은 경우는 i = 0혹은 연속되는 지점에서 발견 가능하다.
-            answer.push(str.substring(pos,pos + TOKEN.length));//!모자이크!를 집어넣는다.
-            i = pos + TOKEN.length;//토큰이 6글자이므로 6이 들어간다.
-        }
-
-        console.log(answer);//확인용 정상작동 되면 지우기
-        return answer;
     };
 
     //좋아요 초기 설정
