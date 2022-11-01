36ac351e4be45dde5256fbbb354a05f244cf72a9
SUH <luckhome5@naver.com>
2022-11-01 10:12:01 +0900

    bug fix 2

--- src/makeStringIntoArray.js
+++ src/makeStringIntoArray.js
@@ -2,7 +2,7 @@ export const makeIntoArray = (str) => {//문자열을 받아서 시작부분이 
     const TOKEN = '!모자이크!';
     let answer = [];
 
-    if(str === null) return ['null'];//이건 문제 해결되면 지우기
+    if(str === null) return ['AI 서버에 문제가 있어서 불러오지 못했습니다.'];//이건 문제 해결되면 지우기
 
     for(let i = 0; i < str.length; i++){
         const pos = str.indexOf(TOKEN, i);//index가 i인 지점부터 확인하여 시간을 줄인다. 토큰은 6글자이다.
@@ -12,7 +12,7 @@ export const makeIntoArray = (str) => {//문자열을 받아서 시작부분이 
         }
         if(i !== pos) answer.push(str.substring(i,pos));//같은 경우는 i = 0혹은 연속되는 지점에서 발견 가능하다.
         answer.push(str.substring(pos,pos + TOKEN.length));//!모자이크!를 집어넣는다.
-        i = pos + TOKEN.length;//토큰이 6글자이므로 6이 들어간다.
+        i = pos + TOKEN.length - 1;//토큰이 6글자이므로 6이 들어간다.
     }
 
     console.log(answer);//확인용 정상작동 되면 지우기
