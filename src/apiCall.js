8b677e3aa5f8d3e6c168452cc4fabee57ccb00b6
SUH <luckhome5@naver.com>
2022-11-02 14:55:55 +0900

    apiCall

---
+++ src/apiCall.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const getAxios = async (url, refreshfunc) => {
+    try {
+        console.log("함수 실행");
+        const res = await axios.get(url);
+        return res;
+    } catch(error) {
+        if(res.response.status === 401 || res.response.status === 0){
+            const message = await refreshfunc();
+            if(message === "refresh success"){
+                const res = await axios.get(url);
+                return res;
+            }
+        }
+    }
+};
+
+export const postAxios = async (url, body, refreshfunc) => {
+    try {
+        const res = await axios.post(url, body);
+        return res;
+    } catch(error) {
+        if(res.response.status === 401 || res.response.status === 0){
+            await refreshfunc();
+            getAxios(url, body, refreshfunc);
+        }
+    }
+};
+
+export const deleteAxios = async (url, refreshfunc) => {
+    try {
+        const res = await axios.delete(url);
+        return res;
+    } catch(error) {
+        if(res.response.status === 401 || res.response.status === 0){
+            await refreshfunc();
+            getAxios(url, refreshfunc);
+        }
+    }
+};
