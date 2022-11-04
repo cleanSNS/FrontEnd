fc7b3a7531c5c76487eac3e8f3ac52b4520f8fa1
Ben <luckhome5@naver.com>
2022-11-04 22:03:14 +0900

    pageList split

---
+++ src/routes/Home/leftInner/pageList/pageListArea.js
@@ -0,0 +1,64 @@
+import Style from './pageList.module.css';
+import { useState, useEffect } from 'react';
+import {
+    getUserPageListUrl,
+} from '../../../../apiUrl';
+import { useInView } from 'react-intersection-observer';
+import { getAxios } from '../../../../apiCall';
+
+const PageListArea = ({loadedUserId, refreshAccessToken, setPageId, setted, leftBookState, setLoading}) => {
+  const [userPageList, setUserPageList] = useState([]);
+  const [pageStartId, setPageStartId] = useState(987654321);
+  const [triger, setTriger] = useState(false);
+  const [lastPageInUserPage, inView] = useInView();
+  const [lastPage, setLastPage] = useState(false);
+
+  //첫 로드시 원래 있던 값들을 초기화해주는 부분
+  const reset = () => {
+      setUserPageList([]);
+      setPageStartId(987654321);
+      setTriger(true);
+  };
+  useEffect(reset, [leftBookState]);
+
+  const presetUserPageList = async () => {
+      setTriger(false);
+      if(!setted) return;
+
+      const res = await getAxios(`${getUserPageListUrl}${loadedUserId}?startId=${pageStartId}`, {}, refreshAccessToken);
+      const tmp = [...res.data.data];
+      if(tmp.length === 0){
+          setLastPage(true);
+      }
+      const currentList = [...userPageList];
+      const next = currentList.concat(tmp);
+      setUserPageList(next);
+      setPageStartId(res.data.startId);
+      setLoading(false);
+  };
+  useEffect(() => {presetUserPageList();}, [triger]);
+
+  const singlePageClickHandler = (event) => {
+      event.preventDefault();
+      setPageId(event.target.id);
+  };
+
+  //무한 로드 함수
+  useEffect(() => {
+      if(inView && !lastPage){
+          presetUserPageList();
+      }
+  }, [inView]);
+
+  return(
+      <div className={Style.pageArea}>
+          {
+              userPageList.map((data, index) => (
+                  <img src={data.imgUrl} className={Style.singlePage} key={index} id={data.pageId} onClick={singlePageClickHandler} ref={userPageList.length - 1 === index ? lastPageInUserPage : null}/>
+              ))
+          }
+      </div>
+  );
+};
+
+export default PageListArea;
---
+++ src/routes/Home/leftInner/pageList/userListArea.js
@@ -0,0 +1,55 @@
+import Style from './pageList.module.css';
+import { useState, useEffect } from 'react';
+import {
+    getFolloweeListUrl,
+    getfollowerListUrl,
+} from '../../../../apiUrl';
+import { getAxios } from '../../../../apiCall';
+
+const UserListArea = ({bottomStuff, refreshAccessToken, leftBookChangeHandler, setted, leftBookState}) => {
+  const [userList, setUserList] = useState([]);
+
+  //페이지 이동 시 초기화함수
+  const reset = () => {
+      setUserList([]);
+  };
+  useEffect(reset, [leftBookState]);
+
+  //팔로워/팔로잉을 불러오는 함수
+  const presetUserListArea = async () => {
+      if(!setted) return;
+      let url = "";
+      bottomStuff === "FOLLOWER" ? 
+      url = getfollowerListUrl
+      :
+      bottomStuff === "FOLLOWEE" ?
+        url = getFolloweeListUrl
+        :
+        url = ""
+      if(url === "") return; //에러상황
+
+      const res = await getAxios(url, {}, refreshAccessToken);
+      setUserList(res.data.data);
+  };
+  useEffect(() => {presetUserListArea();}, [bottomStuff]);
+
+  const userClickHander = (event) => {
+      event.preventDefault();
+      leftBookChangeHandler("pList/" + event.target.id.split('_')[1]);
+  };
+
+  return(
+      <div className={Style.pageArea}>
+          {
+              userList.map((data, index) => (
+                  <div className={Style.userArea} key={index} onClick={userClickHander} id={`pageListUserId_${data.userId}`}>
+                      <img src={data.imgUrl} className={Style.userImg} id={`pageListUserId_${data.userId}`}/>
+                      <p className={Style.userNickname} id={`pageListUserId_${data.userId}`}>{data.nickname}</p>
+                  </div>
+              ))
+          }
+      </div>
+  );
+};
+
+export default UserListArea;
