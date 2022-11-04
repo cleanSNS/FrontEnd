837f2c3f73c5d9cb7e8a84240defd980a542e076
Ben <luckhome5@naver.com>
2022-11-04 22:33:32 +0900

    pageList bug fix trial 8

--- src/routes/Home/leftInner/pageList/pageList.js
+++ src/routes/Home/leftInner/pageList/pageList.js
@@ -13,6 +13,9 @@ import {
 } from '../../../../apiUrl';
 import { getAxios, postAxios } from '../../../../apiCall';
 
+import PageListArea from './pageListArea';
+import UserListArea from './userListArea';
+
 const LeftPageList = ({leftBookState, refreshAccessToken, leftBookChangeHandler, setPageId, userId, SettingChangeHandler, setUserPageAndFriendReloadTriger}) => {//일단 leftBookState를 확인해야한다. pageList/{userId}로 되어있음 userId의 유저 게시글과 이미지, 이름을 불러와서 로딩한다.
     const [userImage, setUserImage] = useState("");//해당 프로필의 유저 이미지
     const [userNickname, setUserNickname] = useState("");//해당 프로필의 유저 닉네임
